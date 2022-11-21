00281d8cc5e9494a03cb01c5586c170468be8660
aprilyanggarwood <yangyyj822@hotmail.com>
2022-11-20 22:47:37 -0800

    edited comments, edited content of about and contact html files

--- week-5/in-n-out-booksp1/src/app/app-routing.module.ts
+++ week-5/in-n-out-booksp1/src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
+/*
+ Title: app-routing.module.ts
+ Author: Professor Krasso
+ Modified By: April Yang
+ Date: 11/20/2022
+ Description: Assignment 5.2 - Navigation and Layout
+Resources: Material Design https://material.io
+*/
+
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
--- week-5/in-n-out-booksp1/src/app/contact/contact.component.ts
+++ week-5/in-n-out-booksp1/src/app/contact/contact.component.ts
@@ -1,3 +1,14 @@
+/*
+ Title: contact.component.ts
+ Author: Professor Krasso
+ Modified By: April Yang
+ Date: 11/20/2022
+ Description: Assignment 5.2 - Navigation and Layout
+Resources: Material Design https://material.io
+*/
+
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
